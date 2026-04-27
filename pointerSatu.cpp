2ff86b4387547f769e6d3156523616cbb597d924
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-27 18:12:19 +0700

    Menambahkan class mahasiswa dengan nim dan showNim

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -7,17 +7,3 @@ class mahasiswa{
         cout<<"No Induk = "<<nim<<endl;
     }
 };
-
-int nim (){
-    mahasiswa mhs{1}; // Object mhs
-    mhs.showNim(); //Member Access Operator 
-
-    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
-    refMhs.nim = 2; //Member Access Operator
-    mhs.showNim();
-
-    mahasiswa *pMhs = &mhs; //pointer Deference pMhs
-    pMhs->nim = 3; //Arrow Operator 
-    pMhs->showNim();
-    return 0;
-}
