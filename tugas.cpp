6aa8610bf61745476e9815e46f3832f8e3325abe
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-27 18:04:07 +0700

    first commit

---
+++ tugas.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+class BelahKetupat;
+class LayangLayang {
+private:
+    //Lengkapi Disini
+
+public:
+    void inputData() {
+        //Lengkapi Disini
+    }
+
+    double hitungLuas() {
+        return 0.5 * d1 * d2;
+    }
+
+
+    //Lengkapi Disini
+}; 
+
+class BelahKetupat {
+private:
+    //Lengkapi Disini 
+}
